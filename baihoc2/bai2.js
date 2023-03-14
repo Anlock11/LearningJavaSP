85c1b84cefffad328a1fde10a08949a043ecae7f
unknown <nguyenanh290402@gmail.com>
2023-03-14 16:29:45 +0700

    bai2

---
+++ baihoc2/bai2.js
@@ -0,0 +1,18 @@
+
+// javasr cộng từ trái sang phải
+let a = 5
+
+console.log(a)
+
+console.log(4+5 + 'a')
+
+console.log('a'+5+4)
+
+// Kiểu String
+
+let st = 'hello anh rất cục súc'
+
+let text = `hello i'm student i love you 
+            but my love is die`
+console.log(st)
+console.log(text)
